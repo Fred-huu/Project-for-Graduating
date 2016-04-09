80525607ed1634c9fee13b19057064debf2c0bb6
Fred-huu <hyd19931224@live.com>
2016-04-09 16:33:05 +0800

    对毕业设计进行重新构造修改

--- Project for Graduating/My Project/Application.Designer.vb
+++ Project for Graduating/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Project_for_Graduating.Form1
+            Me.MainForm = Global.Project_for_Graduating.admin
         End Sub
     End Class
 End Namespace
---
+++ Project for Graduating/admin.Designer.vb
@@ -0,0 +1,393 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class admin
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(admin))
+        Me.aboutPanel = New System.Windows.Forms.Panel()
+        Me.版本信息Label = New System.Windows.Forms.Label()
+        Me.人员姓名Label = New System.Windows.Forms.Label()
+        Me.开发人员Label = New System.Windows.Forms.Label()
+        Me.PictureBox15 = New System.Windows.Forms.PictureBox()
+        Me.开发人员PictureBox = New System.Windows.Forms.PictureBox()
+        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
+        Me.menuPanel = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.UserLabel = New System.Windows.Forms.Label()
+        Me.mess0 = New System.Windows.Forms.PictureBox()
+        Me.including0 = New System.Windows.Forms.PictureBox()
+        Me.user0 = New System.Windows.Forms.PictureBox()
+        Me.admin0 = New System.Windows.Forms.PictureBox()
+        Me.add0 = New System.Windows.Forms.PictureBox()
+        Me.mess1 = New System.Windows.Forms.PictureBox()
+        Me.including1 = New System.Windows.Forms.PictureBox()
+        Me.user1 = New System.Windows.Forms.PictureBox()
+        Me.add1 = New System.Windows.Forms.PictureBox()
+        Me.admin1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.suggestPanel = New System.Windows.Forms.Panel()
+        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
+        Me.aboutPanel.SuspendLayout()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.开发人员PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.menuPanel.SuspendLayout()
+        CType(Me.mess0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.including0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.user0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.admin0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.add0, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.mess1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.including1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.user1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.add1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.admin1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.suggestPanel.SuspendLayout()
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'aboutPanel
+        '
+        Me.aboutPanel.Controls.Add(Me.版本信息Label)
+        Me.aboutPanel.Controls.Add(Me.人员姓名Label)
+        Me.aboutPanel.Controls.Add(Me.开发人员Label)
+        Me.aboutPanel.Controls.Add(Me.PictureBox15)
+        Me.aboutPanel.Controls.Add(Me.开发人员PictureBox)
+        Me.aboutPanel.Controls.Add(Me.PictureBox13)
+        Me.aboutPanel.Location = New System.Drawing.Point(200, 0)
+        Me.aboutPanel.Name = "aboutPanel"
+        Me.aboutPanel.Size = New System.Drawing.Size(1060, 478)
+        Me.aboutPanel.TabIndex = 110
+        '
+        '版本信息Label
+        '
+        Me.版本信息Label.AutoSize = True
+        Me.版本信息Label.Location = New System.Drawing.Point(110, 385)
+        Me.版本信息Label.Name = "版本信息Label"
+        Me.版本信息Label.Size = New System.Drawing.Size(116, 18)
+        Me.版本信息Label.TabIndex = 110
+        Me.版本信息Label.Text = "2.5.160409.3"
+        '
+        '人员姓名Label
+        '
+        Me.人员姓名Label.AutoSize = True
+        Me.人员姓名Label.Location = New System.Drawing.Point(480, 190)
+        Me.人员姓名Label.Name = "人员姓名Label"
+        Me.人员姓名Label.Size = New System.Drawing.Size(62, 54)
+        Me.人员姓名Label.TabIndex = 109
+        Me.人员姓名Label.Text = "颜胜" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "胡永定" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "叶兴"
+        '
+        '开发人员Label
+        '
+        Me.开发人员Label.AutoSize = True
+        Me.开发人员Label.Location = New System.Drawing.Point(110, 190)
+        Me.开发人员Label.Name = "开发人员Label"
+        Me.开发人员Label.Size = New System.Drawing.Size(368, 72)
+        Me.开发人员Label.TabIndex = 108
+        Me.开发人员Label.Text = "汽车衡智能称重系统硬件平台的设计：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "汽车衡智能称重系统数据采集的设计与实现：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "汽车衡智能称重信息管理系统的设计与实现：" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'PictureBox15
+        '
+        Me.PictureBox15.Image = CType(resources.GetObject("PictureBox15.Image"), System.Drawing.Image)
+        Me.PictureBox15.Location = New System.Drawing.Point(110, 310)
+        Me.PictureBox15.Name = "PictureBox15"
+        Me.PictureBox15.Size = New System.Drawing.Size(216, 50)
+        Me.PictureBox15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox15.TabIndex = 107
+        Me.PictureBox15.TabStop = False
+        '
+        '开发人员PictureBox
+        '
+        Me.开发人员PictureBox.Image = CType(resources.GetObject("开发人员PictureBox.Image"), System.Drawing.Image)
+        Me.开发人员PictureBox.Location = New System.Drawing.Point(110, 125)
+        Me.开发人员PictureBox.Name = "开发人员PictureBox"
+        Me.开发人员PictureBox.Size = New System.Drawing.Size(216, 50)
+        Me.开发人员PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.开发人员PictureBox.TabIndex = 106
+        Me.开发人员PictureBox.TabStop = False
+        '
+        'PictureBox13
+        '
+        Me.PictureBox13.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.PictureBox13.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.PictureBox13.Image = CType(resources.GetObject("PictureBox13.Image"), System.Drawing.Image)
+        Me.PictureBox13.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox13.Name = "PictureBox13"
+        Me.PictureBox13.Size = New System.Drawing.Size(1060, 60)
+        Me.PictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox13.TabIndex = 105
+        Me.PictureBox13.TabStop = False
+        '
+        'menuPanel
+        '
+        Me.menuPanel.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.menuPanel.Controls.Add(Me.Label2)
+        Me.menuPanel.Controls.Add(Me.UserLabel)
+        Me.menuPanel.Controls.Add(Me.mess0)
+        Me.menuPanel.Controls.Add(Me.including0)
+        Me.menuPanel.Controls.Add(Me.user0)
+        Me.menuPanel.Controls.Add(Me.admin0)
+        Me.menuPanel.Controls.Add(Me.add0)
+        Me.menuPanel.Controls.Add(Me.mess1)
+        Me.menuPanel.Controls.Add(Me.including1)
+        Me.menuPanel.Controls.Add(Me.user1)
+        Me.menuPanel.Controls.Add(Me.add1)
+        Me.menuPanel.Controls.Add(Me.admin1)
+        Me.menuPanel.Controls.Add(Me.PictureBox1)
+        Me.menuPanel.Location = New System.Drawing.Point(0, 0)
+        Me.menuPanel.Name = "menuPanel"
+        Me.menuPanel.Size = New System.Drawing.Size(200, 700)
+        Me.menuPanel.TabIndex = 111
+        Me.menuPanel.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 628)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 18)
+        Me.Label2.TabIndex = 66
+        Me.Label2.Text = "用户名："
+        '
+        'UserLabel
+        '
+        Me.UserLabel.AutoSize = True
+        Me.UserLabel.Location = New System.Drawing.Point(88, 628)
+        Me.UserLabel.Name = "UserLabel"
+        Me.UserLabel.Size = New System.Drawing.Size(62, 18)
+        Me.UserLabel.TabIndex = 65
+        Me.UserLabel.Text = "用户名"
+        '
+        'mess0
+        '
+        Me.mess0.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.mess0.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.mess0.Image = CType(resources.GetObject("mess0.Image"), System.Drawing.Image)
+        Me.mess0.Location = New System.Drawing.Point(0, 120)
+        Me.mess0.Name = "mess0"
+        Me.mess0.Size = New System.Drawing.Size(200, 60)
+        Me.mess0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.mess0.TabIndex = 11
+        Me.mess0.TabStop = False
+        '
+        'including0
+        '
+        Me.including0.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.including0.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.including0.Image = CType(resources.GetObject("including0.Image"), System.Drawing.Image)
+        Me.including0.Location = New System.Drawing.Point(0, 300)
+        Me.including0.Name = "including0"
+        Me.including0.Size = New System.Drawing.Size(200, 60)
+        Me.including0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.including0.TabIndex = 7
+        Me.including0.TabStop = False
+        '
+        'user0
+        '
+        Me.user0.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.user0.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.user0.Image = CType(resources.GetObject("user0.Image"), System.Drawing.Image)
+        Me.user0.Location = New System.Drawing.Point(0, 241)
+        Me.user0.Name = "user0"
+        Me.user0.Size = New System.Drawing.Size(200, 60)
+        Me.user0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.user0.TabIndex = 8
+        Me.user0.TabStop = False
+        '
+        'admin0
+        '
+        Me.admin0.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.admin0.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.admin0.Image = CType(resources.GetObject("admin0.Image"), System.Drawing.Image)
+        Me.admin0.Location = New System.Drawing.Point(0, 60)
+        Me.admin0.Name = "admin0"
+        Me.admin0.Size = New System.Drawing.Size(200, 60)
+        Me.admin0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.admin0.TabIndex = 10
+        Me.admin0.TabStop = False
+        Me.admin0.Visible = False
+        '
+        'add0
+        '
+        Me.add0.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.add0.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.add0.Image = CType(resources.GetObject("add0.Image"), System.Drawing.Image)
+        Me.add0.Location = New System.Drawing.Point(0, 180)
+        Me.add0.Name = "add0"
+        Me.add0.Size = New System.Drawing.Size(200, 60)
+        Me.add0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.add0.TabIndex = 9
+        Me.add0.TabStop = False
+        '
+        'mess1
+        '
+        Me.mess1.BackColor = System.Drawing.SystemColors.Menu
+        Me.mess1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.mess1.Image = CType(resources.GetObject("mess1.Image"), System.Drawing.Image)
+        Me.mess1.Location = New System.Drawing.Point(0, 120)
+        Me.mess1.Name = "mess1"
+        Me.mess1.Size = New System.Drawing.Size(200, 60)
+        Me.mess1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.mess1.TabIndex = 6
+        Me.mess1.TabStop = False
+        '
+        'including1
+        '
+        Me.including1.BackColor = System.Drawing.SystemColors.Menu
+        Me.including1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.including1.Image = CType(resources.GetObject("including1.Image"), System.Drawing.Image)
+        Me.including1.Location = New System.Drawing.Point(0, 300)
+        Me.including1.Name = "including1"
+        Me.including1.Size = New System.Drawing.Size(200, 60)
+        Me.including1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.including1.TabIndex = 1
+        Me.including1.TabStop = False
+        '
+        'user1
+        '
+        Me.user1.BackColor = System.Drawing.SystemColors.Menu
+        Me.user1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.user1.Image = CType(resources.GetObject("user1.Image"), System.Drawing.Image)
+        Me.user1.Location = New System.Drawing.Point(0, 240)
+        Me.user1.Name = "user1"
+        Me.user1.Size = New System.Drawing.Size(200, 60)
+        Me.user1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.user1.TabIndex = 2
+        Me.user1.TabStop = False
+        '
+        'add1
+        '
+        Me.add1.BackColor = System.Drawing.SystemColors.Menu
+        Me.add1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.add1.Image = CType(resources.GetObject("add1.Image"), System.Drawing.Image)
+        Me.add1.Location = New System.Drawing.Point(0, 180)
+        Me.add1.Name = "add1"
+        Me.add1.Size = New System.Drawing.Size(200, 60)
+        Me.add1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.add1.TabIndex = 3
+        Me.add1.TabStop = False
+        '
+        'admin1
+        '
+        Me.admin1.BackColor = System.Drawing.SystemColors.Menu
+        Me.admin1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.admin1.Image = CType(resources.GetObject("admin1.Image"), System.Drawing.Image)
+        Me.admin1.Location = New System.Drawing.Point(0, 60)
+        Me.admin1.Name = "admin1"
+        Me.admin1.Size = New System.Drawing.Size(200, 60)
+        Me.admin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.admin1.TabIndex = 4
+        Me.admin1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(200, 60)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'suggestPanel
+        '
+        Me.suggestPanel.Controls.Add(Me.PictureBox12)
+        Me.suggestPanel.Location = New System.Drawing.Point(200, 0)
+        Me.suggestPanel.Name = "suggestPanel"
+        Me.suggestPanel.Size = New System.Drawing.Size(1060, 263)
+        Me.suggestPanel.TabIndex = 112
+        '
+        'PictureBox12
+        '
+        Me.PictureBox12.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.PictureBox12.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.PictureBox12.Image = CType(resources.GetObject("PictureBox12.Image"), System.Drawing.Image)
+        Me.PictureBox12.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox12.Name = "PictureBox12"
+        Me.PictureBox12.Size = New System.Drawing.Size(1060, 60)
+        Me.PictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox12.TabIndex = 105
+        Me.PictureBox12.TabStop = False
+        '
+        'admin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1258, 664)
+        Me.Controls.Add(Me.menuPanel)
+        Me.Controls.Add(Me.aboutPanel)
+        Me.Controls.Add(Me.suggestPanel)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "admin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "汽车衡智能称重系统数据采集"
+        Me.aboutPanel.ResumeLayout(False)
+        Me.aboutPanel.PerformLayout()
+        CType(Me.PictureBox15, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.开发人员PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.menuPanel.ResumeLayout(False)
+        Me.menuPanel.PerformLayout()
+        CType(Me.mess0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.including0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.user0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.admin0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.add0, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.mess1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.including1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.user1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.add1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.admin1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.suggestPanel.ResumeLayout(False)
+        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents aboutPanel As Panel
+    Friend WithEvents 版本信息Label As Label
+    Friend WithEvents 人员姓名Label As Label
+    Friend WithEvents 开发人员Label As Label
+    Friend WithEvents PictureBox15 As PictureBox
+    Friend WithEvents 开发人员PictureBox As PictureBox
+    Friend WithEvents PictureBox13 As PictureBox
+    Friend WithEvents menuPanel As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents UserLabel As Label
+    Friend WithEvents mess0 As PictureBox
+    Friend WithEvents including0 As PictureBox
+    Friend WithEvents user0 As PictureBox
+    Friend WithEvents admin0 As PictureBox
+    Friend WithEvents add0 As PictureBox
+    Friend WithEvents mess1 As PictureBox
+    Friend WithEvents including1 As PictureBox
+    Friend WithEvents user1 As PictureBox
+    Friend WithEvents add1 As PictureBox
+    Friend WithEvents admin1 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents suggestPanel As Panel
+    Friend WithEvents PictureBox12 As PictureBox
+End Class
