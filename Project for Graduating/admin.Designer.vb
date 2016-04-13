e67139c5972d6dd68de465942ced2286fec2c81b
Fred-huu <hyd19931224@live.com>
2016-04-13 14:24:34 +0800

    版本号：2.8.160413.1

--- Project for Graduating/admin.Designer.vb
+++ Project for Graduating/admin.Designer.vb
@@ -299,7 +299,7 @@ Partial Class admin
         Me.版本信息Label.Name = "版本信息Label"
         Me.版本信息Label.Size = New System.Drawing.Size(116, 18)
         Me.版本信息Label.TabIndex = 110
-        Me.版本信息Label.Text = "2.7.160412.5"
+        Me.版本信息Label.Text = "2.8.160413.1"
         '
         '人员姓名Label
         '
