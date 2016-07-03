2635ba845a300a90458bd7e0ff798466a2f76354
hyd19 <hyd19@SURFACE>
2016-07-03 21:00:51 +0800

    版本号：2.9.160703.2 生成安装程序

--- Project for Graduating/admin.Designer.vb
+++ Project for Graduating/admin.Designer.vb
@@ -322,7 +322,7 @@ Partial Class admin
         Me.版本信息Label.Name = "版本信息Label"
         Me.版本信息Label.Size = New System.Drawing.Size(116, 18)
         Me.版本信息Label.TabIndex = 110
-        Me.版本信息Label.Text = "2.9.160625.1"
+        Me.版本信息Label.Text = "2.9.160703.2"
         '
         '人员姓名Label
         '
